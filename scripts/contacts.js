38973d49a3e3274dc039f6e0b4d304249d2a3c62
Juho Kaartinen <jai.kaartinen@gmai.com>
2018-10-06 19:53:17 +0300

    Adds character_template.json. Contacts are created dynamically.

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -13,10 +13,41 @@ $.ajax({
   console.log("File data: " + data.contacts);
   for (var i = 0; i < data.contacts.length; i++ ) {
     let cont = data.contacts[i];
-    $("#contacts-ulist").append( "<p>Name: "+cont.name+"<\p>" );
-    $("#contacts-ulist").append( "<p>Likes me: "+cont.likes_me+"<\p>" );
-    $("#contacts-ulist").append( "<p>Influence: "+cont.Influence+"<\p>" );
-    $("#contacts-ulist").append( "<p>Story: "+cont.story+"<\p>" );
+    // Write key-value pairs to HTML
+    for (var key in cont) {
+      // console.log("Key: " + key + ", Value: " + cont[index]);
+      //Keys starting with capital letter are visible in UI
+      if (key[0] == key[0].toUpperCase())
+        $("#contacts-ulist").append( "<p>"+key+": "+cont[key]+"<\p>" );
+    }
+    // Check if contacts has picture path and picture available
+    if (cont.hasOwnProperty("image_path")) {
+      let path = cont["image_path"];
+      // Create button to show and hide image
+      let btn_id = "img-btn-" + i;
+      let img_id = "img-" + i;
+      console.log("Loading picture from " +  path);
+      let btn = $( "<button />", { type:"button", id: btn_id } );
+      $("#contacts-ulist").append(btn);
+      // Create image, hide by default
+      let img = $( "<img />", { src:path, "id": img_id } );
+      $("#contacts-ulist").append(img);
+      //btn.append(img);
+      img.hide();
+      btn.val("Show picture");
+      // Apply callback to button
+      //$( "#"+btn_id ).click(function() {
+      btn.click(function() {
+        if ( img.is( ":visible" )) {
+          img.hide();
+          btn.val("Show picture")
+        }
+        else {
+          img.show();
+          btn.val("Hide picture")
+        }
+      });
+    }
   }
 })
 .fail(function() {
